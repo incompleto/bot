d3336e879d6a3e9e49e28c01c39d118cb4c9d485
Glitch (hello-express) <none>
2019-02-04 21:25:32 +0000

    🌄🎸 Checkpoint ./package.json:305456/116 ./server.js:305456/3204

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const API_KEY=process.env.API_KEY
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
+app.use(bodyParser.json()) 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+) 
+
+app.post('/message', function(req, res) {
+  const { message } = req.body
+
+  console.log(message)
+
+  if (!message) {
+    return res.end()
+  }
+
+  let chat_id = message.chat.id
+  let text = 'Polo'
+  
+  axios
+    .post(`https://api.telegram.org/bot${API_KEY}/sendMessage`, { chat_id, text })
+    .then(response => {
+      res.end('ok')
+    })
+    .catch(err => {
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
+})
+
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
