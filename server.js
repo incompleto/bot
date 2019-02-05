041ec329c8583bec15e1d69518fd2f5af1ef9029
Glitch (hello-express) <none>
2019-02-05 07:10:07 +0000

    🚆🎊 Checkpoint ./server.js:305456/4892

--- server.js
+++ server.js
@@ -5,27 +5,26 @@ const axios = require('axios')
 const getURLS = require('get-urls')
 const Airtable = require('airtable')
 
-
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
 const AIRTABLE_ENDPOINT = process.env.AIRTABLE_ENDPOINT
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID
 const AIRTABLE_BASE = process.env.AIRTABLE_BASE_NAME
+const AIRTABLE_TABLE = process.env.AIRTABLE_TABLE
 
 const API_KEY = process.env.API_KEY
 const TELEGRAM_URL = `https://api.telegram.org/bot${API_KEY}/sendMessage`
 
 Airtable.configure({
-    endpointUrl: AIRTABLE_ENDPOINT,
-    apiKey: AIRTABLE_API_KEY
-  })
+  endpointUrl: AIRTABLE_ENDPOINT,
+  apiKey: AIRTABLE_API_KEY
+})
 
-  let base = new Airtable({ AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID)
+let base = new Airtable({ AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID)
 
-  const showError = (error) => {
-    console.error(error)
-  }
+const showError = (error) => {
+  console.error(error)
+}
 
-  
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
@@ -41,45 +40,45 @@ app.use(
 
 const answerToURLS = (urls, message) => {
   let chat_id = message.chat.id
-  let text = urls.size > 1 ? 'Nice urls!' : 'Nice url!'
+  let text = urls.length > 1 ? 'Nice urls!' : 'Nice url!'
   let data = { chat_id, text }
-  
+
   return axios.post(TELEGRAM_URL, data)
 }
 
 app.post('/message', function(req, res) {
   const { message } = req.body
-  
-  let urls = new Set()
-  
+
+  let urls = []
+
   if (!message) {
     return res.end()
   }
-    
+
   if (message && message.text) {
-    urls = getURLS(message.text)
-  
-    if (urls.size > 0) {
+    urls = Array.from(getURLS(message.text))
+
+    if (urls.length > 0) {
       answerToURLS(urls, message)
-      .then(response => {
+        .then(response => {
+
+          for (let url in urls) {
+              base(AIRTABLE_TABLE).create({ url }, (response) => {
+              console.log(response)
+          })
+
+          }
+
         
-        let a = Array.from(urls);
-        
-        urls = a.join(',')
-        
-        base("Table 1").create({ urls }, (response) => {
-          console.log(response)
+          res.end('ok')
         })
-        
-      res.end('ok')
-      })
-      .catch(err => {
-        console.log('Error :', err)
-        res.end('Error :' + err)
+        .catch(err => {
+          console.log('Error :', err)
+          res.end('Error :' + err)
         })
     }
   }
-  
+
   res.end('ok')
 })
 
