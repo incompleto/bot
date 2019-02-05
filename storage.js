b31e5541b95bd6a93e642f2cd8b8ef4a06f5f055
Javier Arce <javierarce@users.noreply.github.com>
2019-02-05 21:50:12 +0100

    Adds method to allow storing images

--- storage.js
+++ storage.js
@@ -5,7 +5,8 @@ const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
 const AIRTABLE_ENDPOINT = process.env.AIRTABLE_ENDPOINT
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID
 const AIRTABLE_BASE = process.env.AIRTABLE_BASE_NAME
-const AIRTABLE_TABLE = process.env.AIRTABLE_TABLE
+const AIRTABLE_TABLE_URLS = process.env.AIRTABLE_TABLE_URLS
+const AIRTABLE_TABLE_IMAGES = process.env.AIRTABLE_TABLE_IMAGES
 
 Airtable.configure({
   endpointUrl: AIRTABLE_ENDPOINT,
@@ -17,11 +18,16 @@ let base = new Airtable({ AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID)
 const createStorage = () => {
  
   const storeURL = ({ group, username, url }, callback) => {
-    base(AIRTABLE_TABLE).create({ group, username, url }, callback)
+    base(AIRTABLE_TABLE_URLS).create({ group, username, url }, callback)
+  }
+  
+  const storeImage = ({ group, username, image }, callback) => {
+    base(AIRTABLE_TABLE_IMAGES).create({ group, username, image }, callback)
   }
   
   return {
-    storeURL
+    storeURL,
+    storeImage
   }
 }
 
